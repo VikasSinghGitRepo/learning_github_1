8a94af54114c1570465ad22eaf6a67cc6ea84807
VikasSinghGitRepo <04092001vs@gmail.com>
2022-08-19 00:32:07 +0530

    The First Commit

---
+++ HelloWorld1/src/class1.java
@@ -0,0 +1,11 @@
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HII this is my"
+				+ " first program in java in learning github");
+
+	}
+
+}
